c194da696e0840c003ffd8ed483ffb9363fb65ab
Anjury Patawaran <anjurypatawaran02@gmail.com>
2024-03-19 13:19:55 +0800

    added add to cart and favorite

--- resources/js/Pages/Shop/Show.jsx
+++ resources/js/Pages/Shop/Show.jsx
@@ -1,20 +1,23 @@
 import Box from "@/Components/Box";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { usePage } from "@inertiajs/react";
+import { Link, usePage } from "@inertiajs/react";
+import ShopNavLink from "./ShopNavLink";
+import SecondaryButton from "@/Components/SecondaryButton";
+import { HeartIcon } from "@heroicons/react/24/outline";
 
 export default function Show({ auth })
 {
     const { product_detail } = usePage().props
     const { category } = usePage().props
     const { products } = usePage().props
-    
+
     console.log(product_detail);
     return (
         <AuthenticatedLayout
             user={ auth.user }
         >
             <ShopNavLink category={ category } products={ products } />
-            
+
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
@@ -37,10 +40,8 @@ export default function Show({ auth })
                             <div className="md:col-span-1 flex flex-col gap-4">
                                 <Box>
                                     Product info
-                                    
-                                    <div className="text-2xl font-bold">
-                                        ₱ { product_detail.price }
-                                    </div>
+
+
                                     <div className="text-lg pt-2">
                                         { product_detail.name }
                                     </div>
@@ -48,18 +49,32 @@ export default function Show({ auth })
                                         { product_detail.description }
                                     </div>
 
-                                    <div className="pt-4">
+                                    <div className="text-2xl font-bold pt-4">
+                                        ₱ { product_detail.price }
+                                    </div>
+
+                                    <div className="pt-4 pb-4">
                                         <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
                                             Stock: { product_detail.quantity }
                                         </span>
                                     </div>
+
+                                    <Link className="mt-4 w-full flex items-center justify-center gap-2">
+                                        <SecondaryButton className="w-4/6 items-center justify-center hover:bg-gray-800 hover:text-white">
+                                            Add to cart
+                                        </SecondaryButton>
+                                        <SecondaryButton className="w-2/6 items-center justify-center hover:bg-gray-800 hover:text-white">
+                                            <HeartIcon className="w-4"/>
+                                        </SecondaryButton>
+                                    </Link>
+
                                     {/* <Price :price="listing.price" class="text-2xl font-bold" />
                                     <ListingSpace :listing="listing" class="text-lg" />
                                     <ListingAddress :listing="listing" class="text-gray-500" /> */}
                                 </Box>
 
                                 <Box>
-                                    Monthly Payment
+
                                     {/* <div>
                                         <label class="label">Interest Rate ({{ interestRate }}%)</label>
                                         <input v-model.number="interestRate" type="range" min="0.1" max="30" step="0.1" class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" />
